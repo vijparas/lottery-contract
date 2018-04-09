08c59cb2a951caacad590e5ba54597fd113a9b46
Paras <vijparas@gmail.com>
2018-04-09 23:57:01 +0530

    Created a new contract for lottery. We have one contract Lottery.sol which contains the code for our contract. We have compile.js script for generating the bytecode and interface of our contract. deploy.js for deploying the contract into rinkeby network. We also have test to test out contract in local ganache test environment

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -5,27 +5,37 @@ contract Lottery{
     address[] public players;
 
     function Lottery( ) public {
+      // manager is intialized with address of person who has created contract
         manager=msg.sender;
     }
+    // payable is a kayword that represents that a transaction will take place and so some ether will be spent.
     function enter() public payable{
         require(msg.value>.01 ether);
         players.push(msg.sender);
     }
-
+    // view is a keyword that tells that functions will not make any modification
     function random() private view returns(uint){
+      //keccak256 is alias for sha3
         return uint(keccak256(block.difficulty,now,players));
    }
  function pickWinner() public restricted{
-
+   //Picking a winner by calling the random generator function and taking module of it with total players.
      uint winner=random()% players.length;
      players[winner].transfer(address(this).balance);
+     // resetting the players array. new address[](0) represents that array is a dynamic array of type address
+     // with total values 0.
      players=new address[](0);
  }
+ // Modifier is used to allow us to remove code duplicacy of code. So if many functions are calling
+ // a functionality we can create a modifier for it and call that modifier during function definition
+ //function pickWinner() public restricted . The restricted here is the modifier
  modifier restricted(){
+   // msg.sender is used to fetch address of person who is calling function
      require(msg.sender==manager);
+
      _;
  }
-
+// This function return array of players.
  function getPlayers( ) public view returns(address[]){
      return players;
  }
